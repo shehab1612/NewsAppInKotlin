2447077cf421adcb1ee9cd0e89b44c69479f076e
youssef <youssef99.shawky@gmail.com>
2020-10-06 01:50:10 +0200

    edit on the api interface

--- app/src/main/java/com/example/newsappinkotlin/APIServices/ApiInterface.kt
+++ app/src/main/java/com/example/newsappinkotlin/APIServices/ApiInterface.kt
@@ -6,10 +6,10 @@ import retrofit2.http.Query
 interface ApiInterface {
     @GET("v2/everything")
     fun getCountry(@Query("api_Key")apiKey:String="4451200787294791a3863b9dcd8c9903",
-                   @Query("country")country:String="eg",
+                   @path("country")country:String="eg",
                    @Query("page") pageNumber: Int = 1): Call<NewsModel>
     @GET("v2/top-headlines")
     fun getTopic(@Query("api_Key")apiKey:String="4451200787294791a3863b9dcd8c9903",
-                 @Query("q")topic:String="all",
+                 @path("q")topic:String="all",
                  @Query("page") pageNumber: Int = 1):Call<NewsModel>
 }
