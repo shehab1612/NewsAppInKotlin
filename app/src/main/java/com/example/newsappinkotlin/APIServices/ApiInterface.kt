37092169041d5dbe6ac1eea9a4e219320d5a08f5
youssef <youssef99.shawky@gmail.com>
2020-10-06 01:45:15 +0200

    I added the Api Interface

---
+++ app/src/main/java/com/example/newsappinkotlin/APIServices/ApiInterface.kt
@@ -0,0 +1,15 @@
+package com.example.newsappinkotlin.APIServices
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiInterface {
+    @GET("v2/everything")
+    fun getCountry(@Query("api_Key")apiKey:String="4451200787294791a3863b9dcd8c9903",
+                   @Query("country")country:String="eg",
+                   @Query("page") pageNumber: Int = 1): Call<NewsModel>
+    @GET("v2/top-headlines")
+    fun getTopic(@Query("api_Key")apiKey:String="4451200787294791a3863b9dcd8c9903",
+                 @Query("q")topic:String="all",
+                 @Query("page") pageNumber: Int = 1):Call<NewsModel>
+}
