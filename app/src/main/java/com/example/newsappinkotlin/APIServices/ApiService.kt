0a3f7cbd1e77a150e84fcfa2d147b4d3970894d6
omar <omark897@gmail.com>
2020-10-05 15:10:27 +0200

    added api services interface

---
+++ app/src/main/java/com/example/newsappinkotlin/APIServices/ApiService.kt
@@ -0,0 +1,21 @@
+package com.example.newsappinkotlin.APIServices
+
+import com.google.gson.annotations.SerializedName
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+import retrofit2.http.Path
+interface ApiService {
+
+
+//gets top headlines by country called and will be defaulted to egypt in cas no country chosen from  spinner
+    @GET("top-headlines?country={country}")
+    fun getByCountry(@Query("apiKey") apiKey:String="4451200787294791a3863b9dcd8c9903",
+    @Path("country")country:String="eg",@Query("page")page:Int=1): Call<CallResponse>
+    //gets news about what the user searches for and will be defaulted to all topics
+    @GET("/everythong?q={topic}")
+    fun getByTopic(@Query("apiKey") apiKey:String="4451200787294791a3863b9dcd8c9903",
+                   @Path("topic")topic:String="all"
+    ,@Query("page")page:Int=1): Call<CallResponse>
+
+}
