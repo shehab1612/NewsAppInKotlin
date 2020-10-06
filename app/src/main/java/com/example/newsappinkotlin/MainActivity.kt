4a9809d05479358644890d6a041a210e07f8607f
shehab1612 <65543128+shehab1612@users.noreply.github.com>
2020-10-06 14:27:05 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/newsappinkotlin/MainActivity.kt
+++ app/src/main/java/com/example/newsappinkotlin/MainActivity.kt
@@ -13,5 +13,55 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         bottom_nav_view.setupWithNavController(findNavController(R.id.nav_host_fragment_container))
+        
+        val headlines_fragment = HeadlinesFragment()
+        val itemDetails_fragment = ItemDetailsFragment()
+        val savedItems_fragment = SavedItemsFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.nav_host_fragment_container, headlines_fragment)
+            addToBackStack(null)
+            commit()
+        }
+
+        bottom_nav_view.headlinesFragment.SetOnClickListener{
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.nav_host_fragment_container, headlines_fragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        bottom_nav_view.savedItemsFragment.SetOnClickListener{
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.nav_host_fragment_container, savedItems_fragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        imageButton.SetOnClickListener{
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.nav_host_fragment_container, headlines_fragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        imageView2.SetOnClickListener{
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.nav_host_fragment_container, itemDetails_fragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        imageButton2.SetOnClickListener{
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.nav_host_fragment_container, savedItems_fragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
     }
 }
