d2f73601ae27afd985e1e565b242ac1597070596
Marwan <marwanelmessiry2000@gmail.com>
2020-10-07 22:26:27 +0200

    - First Implementation of Room database. - Added the interface and abstract required classes. - Modified the model data class by adding a database table entity.

--- app/src/main/java/com/example/newsappinkotlin/APIServices/NewsModel.kt
+++ app/src/main/java/com/example/newsappinkotlin/APIServices/NewsModel.kt
@@ -1,8 +1,10 @@
 package com.example.newsappinkotlin.APIServices
 
+import androidx.room.Entity
 import com.google.gson.annotations.SerializedName
 import java.util.*
 
+@Entity(tableName = "news_table")
 data class NewsModel
     (
     @SerializedName ("source") val source: mySource,
---
+++ app/src/main/java/com/example/newsappinkotlin/database/AppDatabase.kt
@@ -0,0 +1,27 @@
+package com.example.newsappinkotlin.database
+
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+abstract class AppDatabase: RoomDatabase(){
+    abstract fun getMovieDao(): MoviesDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase{
+            val tempInstance = INSTANCE
+            if(tempInstance != null)
+                return tempInstance
+            synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext, AppDatabase::class.java, "news_db"
+                ).allowMainThreadQueries().build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
