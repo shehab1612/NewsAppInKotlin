53185eb0b58b11ae4d024466452da079f0fefc78
omar <omark897@gmail.com>
2020-10-10 20:52:36 +0200

    adjusted some issues

--- app/src/main/java/com/example/newsappinkotlin/APIServices/ApiService.kt
+++ app/src/main/java/com/example/newsappinkotlin/APIServices/ApiService.kt
@@ -10,13 +10,13 @@ interface ApiService {
 
     //gets top headlines by country called and will be defaulted to egypt in cas no country chosen from  spinner
     @GET("v2/top-headlines")
-      fun getByCountry(@Query("apiKey") apiKey:String="4451200787294791a3863b9dcd8c9903",
+      fun getByCountry(@Query("apiKey") apiKey:String="a728f12c39f9481bb852dab2d4547c00",
                        @Query("country")country:String="eg"): Call<CallResponse>
     //gets news about what the user searches for and will be defaulted to all topics
     @GET("v2/everything")
     fun getByTopic(
         @Query("q")topic:String="all",
-        @Query("apiKey") apiKey:String="4451200787294791a3863b9dcd8c9903"
+        @Query("apiKey") apiKey:String="a728f12c39f9481bb852dab2d4547c00"
 
          ,@Query("page")page:Int=1): Call<CallResponse>
 
