7a48a1a8b36aad31644e9112c343522f860d93d1
omar <omark897@gmail.com>
2020-10-14 01:07:38 +0200

    cloned the clicklistener of save button to be able to save through the item details list

--- app/src/main/java/com/example/newsappinkotlin/ui/destinations/ItemDetailsFragment.kt
+++ app/src/main/java/com/example/newsappinkotlin/ui/destinations/ItemDetailsFragment.kt
@@ -1,26 +1,34 @@
 package com.example.newsappinkotlin.ui.destinations
 
 import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Bundle
 import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.example.newsappinkotlin.APIServices.NewsModel
 import com.example.newsappinkotlin.R
+import com.example.newsappinkotlin.database.AppDatabase
+import kotlinx.android.synthetic.main.fragment_headlines.view.*
 import kotlinx.android.synthetic.main.fragment_item_details.*
 import kotlinx.android.synthetic.main.fragment_saved_items.*
 import kotlinx.android.synthetic.main.news_card.view.*
+import java.io.ByteArrayOutputStream
 import java.text.SimpleDateFormat
 import java.util.*
 
 class ItemDetailsFragment : Fragment() {
-
+    lateinit var dataBase: AppDatabase
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -45,7 +53,27 @@ class ItemDetailsFragment : Fragment() {
         val date = dateTimeFormat.parse(newsModel.Date)
         dateTimeTextView.text = date.toString()
 
+        save.setOnClickListener {
+            dataBase = AppDatabase.getDatabase(this.requireContext())
+            val newsmodel = newsModel
+            Glide.with(this.requireContext())
+                .asBitmap()
+                .load(newsmodel?.Image)
+                .into(object : CustomTarget<Bitmap>(500,500) {
+                    override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                        val stream = ByteArrayOutputStream()
 
+                        resource.compress(Bitmap.CompressFormat.PNG, 100, stream)
+
+                        newsmodel?.savedImage = stream.toByteArray()
+                        newsmodel?.let { it1 -> dataBase.getNewsDao().insertNews(it1) }
+
+                    }
+
+                    override fun onLoadCleared(placeholder: Drawable?) {}
+                })
+            Toast.makeText(this.requireContext(),"SAVED", Toast.LENGTH_SHORT).show()
+        }
         //Read full click listener
         readFullButton.setOnClickListener {
             val url = newsModel.url
