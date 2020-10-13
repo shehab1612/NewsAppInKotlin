833d523da350d3464b6b980520c09c6dc659b4a8
omar <omark897@gmail.com>
2020-10-13 21:01:04 +0200

    dont take this commit just to pull updates

--- app/src/main/java/com/example/newsappinkotlin/APIServices/NewsModel.kt
+++ app/src/main/java/com/example/newsappinkotlin/APIServices/NewsModel.kt
@@ -2,6 +2,7 @@ package com.example.newsappinkotlin.APIServices
 
 import android.widget.ImageView
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 import java.util.*
@@ -12,14 +13,17 @@ data class NewsModel
     //@SerializedName ("source") val source: mySource,
     @SerializedName ("author") val author: String,
     @SerializedName ("title") val title: String,
+
+    @PrimaryKey
     @SerializedName ("url") val url: String,
     @SerializedName ("urlToImage") val Image: String,
     @SerializedName ("publishedAt") val Date: String,
-    @SerializedName ("content") val content: String,
+    @SerializedName ("content") val content: String
   //for offline supported image
-    var savedImage:ImageView) : Serializable
+    //var savedImage:ImageView
+    ) : Serializable
      {
          init {
-             savedImage=ImageView(null)
+            // savedImage=ImageView(null)
          }
 }
--- app/src/main/java/com/example/newsappinkotlin/database/AppDatabase.kt
+++ app/src/main/java/com/example/newsappinkotlin/database/AppDatabase.kt
@@ -1,20 +1,25 @@
 package com.example.newsappinkotlin.database
 
 import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.newsappinkotlin.APIServices.NewsModel
 
+@Database(entities = [NewsModel::class], version = 1, exportSchema = false)
 abstract class AppDatabase: RoomDatabase(){
     abstract fun getNewsDao(): NewsDao
 
     companion object{
         @Volatile
-        private var INSTANCE: AppDatabase? = null
+         var INSTANCE: AppDatabase? = null
 
         fun getDatabase(context: Context): AppDatabase{
             val tempInstance = INSTANCE
             if(tempInstance != null)
+            {
                 return tempInstance
+            }
             synchronized(this){
                 val instance = Room.databaseBuilder(
                     context.applicationContext, AppDatabase::class.java, "news_db"
